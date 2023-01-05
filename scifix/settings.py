233cf34bda6f8a9fbb7835143772b73bb9e58f6e
David Ekong <davidukemeekong1@gmail.com>
2023-01-05 23:44:43 +0100

    new

--- scifix/settings.py
+++ scifix/settings.py
@@ -139,9 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
