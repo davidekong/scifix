fd2dcde7ca2d978df14ebb25e746abf5dbfad302
David Ekong <davidukemeekong1@gmail.com>
2023-01-05 23:34:05 +0100

    Media

--- scifix/settings.py
+++ scifix/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/blog/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
