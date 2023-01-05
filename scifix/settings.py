5a2867aac03b9e607b9c6dc1b455a432d9289796
David Ekong <davidukemeekong1@gmail.com>
2023-01-06 00:21:39 +0100

    new

--- scifix/settings.py
+++ scifix/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/blog/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
