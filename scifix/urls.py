a31e9d7f76668fc737503055e1cec5e57f27a896
David Ekong <davidukemeekong1@gmail.com>
2023-01-06 12:56:04 +0100

    sitemaps1

--- scifix/urls.py
+++ scifix/urls.py
@@ -15,13 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 from django.conf import settings
+from blog.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), 
     path('accounts/', include('allauth.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
 
 
