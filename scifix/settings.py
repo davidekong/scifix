34b3ac86763961aa3dc555642a7f5e88cc2102d3
David Ekong <davidukemeekong1@gmail.com>
2023-01-13 00:01:56 +0100

    robot

--- scifix/settings.py
+++ scifix/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'cms',
     'menus',
     'treebeard',
+    'robots'
 ]
 
 
@@ -76,7 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'scifix.urls'
